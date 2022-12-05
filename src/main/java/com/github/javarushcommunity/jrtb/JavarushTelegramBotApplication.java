fec8027e9ccedec33828a1040081252570adc0e5
neighborstan <dropweb.post@gmail.com>
2022-12-06 00:32:36 +0600

    changed properties in files for bot

--- src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
+++ src/main/java/com/github/javarushcommunity/jrtb/JavarushTelegramBotApplication.java
@@ -2,8 +2,10 @@ package com.github.javarushcommunity.jrtb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-dev.properties")
 public class JavarushTelegramBotApplication {
 
 	public static void main(String[] args) {
