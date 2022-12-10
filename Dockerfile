6c4ff5b7bc4e8b15c79b0d42ce8e21d9687df9ae
neighborstan <dropweb.post@gmail.com>
2022-12-10 18:29:56 +0600

    STEP_5_JRTB-1: added DB to docker-compose and created docker-compose-dev.yml for test purposes (#21)
    * STEP_5_JRTB-1: added DB to docker-compose and created docker-compose-dev.yml for test purposes
    
    * STEP_5_JRTB-1: added DB to docker-compose and created docker-compose-dev.yml for test purposes
    
    * STEP_5_JRTB-1: updated application-dev.properties
    
    * STEP_5_JRTB-1: deleted JavarushTelegramBotApplicationTests test class

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 FROM tomcat:9.0.65-jdk17-corretto
+
 ARG JAR_FILE=target/*.jar
+
 ENV BOT_NAME=bot_name
 ENV BOT_TOKEN=bot_token
+ENV BOT_DB_USERNAME=jrtb_db_user
+ENV BOT_DB_PASSWORD=jrtb_db_password
+
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-jar", "/app.jar"]
+
+ENTRYPOINT ["java", "-Dspring.datasource.password=${BOT_DB_PASSWORD}", "-Dspring.datasource.username=${BOT_DB_USERNAME}", "-Dbot.username=${BOT_NAME}", "-Dbot.token=${BOT_TOKEN}", "-jar", "/app.jar"]
--- src/test/java/com/github/javarushcommunity/jrtb/command/CommandContainerTest.java
+++ src/test/java/com/github/javarushcommunity/jrtb/command/CommandContainerTest.java
@@ -2,6 +2,7 @@ package com.github.javarushcommunity.jrtb.command;
 
 
 import com.github.javarushcommunity.jrtb.service.SendBotMessageService;
+import com.github.javarushcommunity.jrtb.service.TelegramUserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,7 +19,8 @@ class CommandContainerTest {
     @BeforeEach
     public void init() {
         SendBotMessageService sendBotMessageService = Mockito.mock(SendBotMessageService.class);
-        commandContainer = new CommandContainer(sendBotMessageService);
+        TelegramUserService telegramUserService = Mockito.mock(TelegramUserService.class);
+        commandContainer = new CommandContainer(sendBotMessageService, telegramUserService);
     }
 
     @Test
