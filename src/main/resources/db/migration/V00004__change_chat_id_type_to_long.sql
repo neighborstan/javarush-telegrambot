fa08c4ceec7391375ebe5c39d5773b5f594c82e1
neighborstan <dropweb.post@gmail.com>
2022-12-23 21:31:19 +0600

    STEP_10: v1.0.0 Implemented all the logic, planned up to MVP: *   User can subscribe on group of posts *   User can view list of group subscriptions on which user subscribes *   User can unsubscribe from group of posts *   User can set an inactive bot and do not receive notifications *   User can restart getting notifications *   Admin has ability to see bot statistics

---
+++ src/main/resources/db/migration/V00004__change_chat_id_type_to_long.sql
@@ -0,0 +1,6 @@
+ALTER TABLE group_x_user DROP FOREIGN KEY group_x_user_ibfk_1;
+
+ALTER TABLE tg_user MODIFY chat_id INT;
+ALTER TABLE group_x_user MODIFY user_id INT;
+
+ALTER TABLE group_x_user ADD FOREIGN KEY (user_id) REFERENCES tg_user (chat_id);
